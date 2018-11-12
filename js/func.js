75ff5a6b866dc24515e980157d86970180c865d0
Jolken <jolken122@gmail.com>
2018-11-12 22:47:14 +0200

    xml config

--- js/func.js
+++ js/func.js
@@ -4,6 +4,25 @@ let funcs = {
         index: async () => {
             return {title:'audio server', text: 'in progress'}
         },
+        xml: async (data) => {
+            return `
+<ezstream>
+    <url>${data.url}</url>
+    <sourcepassword>${data.password}</sourcepassword>
+    <format>MP3</format>
+    <shuffle>${data.shuffle}</shuffle>
+    <filename>${data.playlist}</filename>
+    <stream_once>0</stream_once><svrinfoname>${data.streamName}</svrinfoname>
+    <svrinfourl>${data.infoUrl}</svrinfourl>
+    <svrinfogenre>${data.theme}</svrinfogenre>
+    <svrinfodescription>${data.description}</svrinfodescription>
+    <svrinfobitrate>${data.bitrate}</svrinfobitrate>
+    <svrinfochannels>2</svrinfochannels>
+    <svrinfosamplerate>44100</svrinfosamplerate>
+    <svrinfopublic>0</svrinfopublic>
+</ezstream>
+            `
+        },
     },
     db: {
         get: async (dbName) => {
--- js/routes.js
+++ js/routes.js
@@ -3,6 +3,16 @@ const func = require('./func');
 const child = require('./child');
 const util = require('util')
 let childs = {}
+xmlconf = {
+  url: 'http://127.0.0.1:8000/jusic/',
+  password: 'qwerty',
+  shuffle: 1,
+  playlist: '/home/jolken/music/playlist.m3u',
+  streamName: 'Just music',
+  infoUrl: 'none',
+  theme: 'metal/mixed',
+  description: 'my radio :)',
+}
 module.exports = (app) => {
   app.get('/', async (req, res) => {
     res.render(APP_ROOT + '/public/index', await func.render.index());
@@ -11,13 +21,13 @@ module.exports = (app) => {
   app.get('/db', async (req, res) => {
     await db.open(APP_ROOT+'/db/base.db')
     let sql = `SELECT * FROM test;`
-    let data;
-    data = await db.all(sql)
+    let data = await db.all(sql);
     res.send(data)
   });
 
   app.get('/:proc/start', async (req, res) => {
-    childs[req.params.proc] = child('ezstream', ['-c', '/etc/ezstream/jusic.xml']);
+    config = req.body.config || '/home/jolken/frenki/jusic.xml';
+    childs[req.params.proc] = child('ezstream', ['-c', config]);
     res.send('try to start');
   });
   app.get('/:proc/kill', async (req,res) => {
